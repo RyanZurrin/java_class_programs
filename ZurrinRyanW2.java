48bdd2feaab887e125943163549d7abc5d877b4d
Ryan Zurrin <ryanzurrin@ryanzurrin.com>
2020-09-12 07:09:53 -0400

    chapter 1, program #1
    making sure we have the jave sre installed and that the JAVA_HOME and all enviroment viariables are set properly. There is Documentation here -> https://www.java.com/en/download/help/path.xml on how to accomplish this if anyone needs help with that. We are using an older jre for this class, that being the v8u261 which can be downloaded and installed from here -> https://www.oracle.com/java/technologies/javase/javase-jdk8-downloads.html. I will use the commit to specify details on each program and I don't mind any kind of feedback or constructive critisism is welcome. I am new to java and to programming so welcome any chance to optimize my thinking and coding.

---
+++ ZurrinRyanW2.java
@@ -0,0 +1,16 @@
+//	Program Name: 	ZurrinRyanW2.java
+//	Author: 		Ryan Zurrin
+//	Assignment #:	Practice Assignment
+//	Due date: 		Thursday, September 10, 2020, 3:00 PM
+/*	Description:	Practice program from page 42 of our Java text book,
+					making sure we can save and compile a program. */
+
+// creating a class to print out multiple lines of text to the console
+
+public class ZurrinRyanW2 {
+	// main method to run java application
+	public static void main(String[] args) {
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming!");
+	}//end method main
+}//end class ZurrinRyanW2
